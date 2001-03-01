7e6f720939ef851f05b50c561c7700d3f92228c0
Bruce Guenter <bruce@untroubled.org>
2001-03-01 01:50:33 +0000

    Added tests for the put[ui]w functions.

--- iobuf/test.c
+++ iobuf/test.c
@@ -3,9 +3,13 @@
 int main(void)
 {
   obuf buf;
-  obuf_init(&buf, 2, 0, 10);
+  obuf_init(&buf, 1, 0, 10);
   obuf_puts(&buf, "testing...");
   obuf_puts(&buf, " 1, 2, 3");
   obuf_putsflush(&buf, "...\n");
+  obuf_putiw(&buf,  10, 10, ' '); obuf_putsflush(&buf, "\n");
+  obuf_putiw(&buf,  10, 10, '0'); obuf_putsflush(&buf, "\n");
+  obuf_putiw(&buf, -10, 10, ' '); obuf_putsflush(&buf, "\n");
+  obuf_putiw(&buf, -10, 10, '0'); obuf_putsflush(&buf, "\n");
   return 0;
 }
