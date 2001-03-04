81ce9ca40b028fef0269470f08bf2dfe0101f4b2
Bruce Guenter <bruce@untroubled.org>
2001-03-04 11:06:17 +0000

    Take into account timeout.

--- iobuf/ibuf_getc.c
+++ iobuf/ibuf_getc.c
@@ -6,7 +6,7 @@ int ibuf_getc(ibuf* in, char* ch)
   io = &(in->io);
   in->count = 0;
   if (io->bufstart >= io->buflen) ibuf_refill(in);
-  if (ibuf_eof(in) || ibuf_error(in)) return 0;
+  if (ibuf_eof(in) || ibuf_error(in) || ibuf_timedout(in)) return 0;
   *ch = io->buffer[io->bufstart++];
   in->count = 1;
   return 1;
