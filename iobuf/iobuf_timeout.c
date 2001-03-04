95fbd99705702a349e407e1a59c79c187e340099
Bruce Guenter <bruce@untroubled.org>
2001-03-04 11:05:52 +0000

    Fixed the logic used so it will actually return, and simplified the loop.

--- iobuf/iobuf_timeout.c
+++ iobuf/iobuf_timeout.c
@@ -5,20 +5,17 @@
 int iobuf_timeout(iobuf* io, int poll_out)
 {
   struct pollfd pfd;
-
+  int result;
+  
   if (!io->timeout) return 1;
 
   pfd.fd = io->fd;
   pfd.events = poll_out ? POLLOUT : POLLIN;
-  for (;;) {
-    switch (poll(&pfd, 1, io->timeout)) {
-    case -1:
-      if (errno == EAGAIN) continue;
-      IOBUF_SET_ERROR(io);
-    case 0:
-      io->flags |= IOBUF_TIMEOUT;
-      return 0;
-    }
+  while ((result = poll(&pfd, 1, io->timeout)) == -1) {
+    if (errno == EAGAIN) continue;
+    IOBUF_SET_ERROR(io);
   }
-  return 1;
+  if (result) return 1;
+  io->flags |= IOBUF_TIMEOUT;
+  return 0;
 }
