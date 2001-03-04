51aa07a54138ebbfcea53e4b0cd00cfc79877e57
Bruce Guenter <bruce@untroubled.org>
2001-03-04 11:10:13 +0000

    Take into account timeout in refill. Simplified some logic.

--- iobuf/ibuf_gets.c
+++ iobuf/ibuf_gets.c
@@ -3,13 +3,18 @@
 int ibuf_gets(ibuf* in, char* data, unsigned datalen, char boundary)
 {
   iobuf* io;
+  int ch;
+  
   io = &in->io;
   in->count = 0;
   while (datalen) {
     if (io->bufstart >= io->buflen) ibuf_refill(in);
+    if (ibuf_timedout(in)) return 0;
     if (ibuf_eof(in) || ibuf_error(in)) break;
     in->count++;
-    if ((*data++ = io->buffer[io->bufstart++]) == boundary) return 1;
+    ch = io->buffer[io->bufstart++];
+    *data++ = ch;
+    if (ch == boundary) return 1;
   }
   return ibuf_eof(in);
 }
