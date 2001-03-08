f943573980293ba4e9db68cf3e3097f5f84b624e
Bruce Guenter <bruce@untroubled.org>
2001-03-08 21:43:26 +0000

    Added new multi-string catenation functions.

---
+++ str/cat4s.c
@@ -0,0 +1,37 @@
+/* str/cat4s.c - Append four C strings to a dynamic str
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <string.h>
+#include "str.h"
+
+int str_cat4s(str* s, const char* a, const char* b, const char* c,
+	      const char* d)
+{
+  char* ptr;
+  unsigned alen = strlen(a);
+  unsigned blen = strlen(b);
+  unsigned clen = strlen(c);
+  unsigned dlen = strlen(d);
+  if (!str_realloc(s, s->len+alen+blen+clen+dlen)) return 0;
+  ptr = s->s + s->len;
+  memcpy(ptr, a, alen); ptr += alen;
+  memcpy(ptr, b, blen); ptr += blen;
+  memcpy(ptr, c, clen); ptr += clen;
+  memcpy(ptr, d, dlen); ptr += dlen;
+  *ptr = 0;
+  return 1;
+}
---
+++ str/cat5s.c
@@ -0,0 +1,39 @@
+/* str/cat5s.c - Append five C strings to a dynamic str
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <string.h>
+#include "str.h"
+
+int str_cat5s(str* s, const char* a, const char* b, const char* c,
+	      const char* d, const char* e)
+{
+  char* ptr;
+  unsigned alen = strlen(a);
+  unsigned blen = strlen(b);
+  unsigned clen = strlen(c);
+  unsigned dlen = strlen(d);
+  unsigned elen = strlen(e);
+  if (!str_realloc(s, s->len+alen+blen+clen+dlen+elen)) return 0;
+  ptr = s->s + s->len;
+  memcpy(ptr, a, alen); ptr += alen;
+  memcpy(ptr, b, blen); ptr += blen;
+  memcpy(ptr, c, clen); ptr += clen;
+  memcpy(ptr, d, dlen); ptr += dlen;
+  memcpy(ptr, e, elen); ptr += elen;
+  *ptr = 0;
+  return 1;
+}
