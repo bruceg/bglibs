3060e87a70c30dbde6b41b7edbce6f67c4cc7133
Bruce Guenter <bruce@untroubled.org>
2001-03-29 03:03:05 +0000

    Added string comparison functions.

---
+++ str/cmp.c
@@ -0,0 +1,39 @@
+/* str/cmp.c - Compare part of two strings
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <string.h>
+#include "str.h"
+
+int str_cmp(const str* a, unsigned aoffset, const str* b, unsigned boffset)
+{
+  return str_cmpb(a, aoffset, b->s+boffset, b->len-boffset);
+}
+
+int str_cmps(const str* a, unsigned offset, const char* b)
+{
+  return str_cmpb(a, offset, b, strlen(b));
+}
+
+int str_cmpb(const str* a, unsigned offset, const char* b, unsigned len)
+{
+  char* aptr;
+  if (len + offset > a->len)
+    return 1;
+  for (aptr = a->s; len; ++aptr, ++b, --len)
+    if (*b - *aptr) return *b - *aptr;
+  return 0;
+}
---
+++ str/diff.c
@@ -0,0 +1,42 @@
+/* str/diff.c - Differentiate two strings
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <string.h>
+#include "str.h"
+
+int str_diff(const str* a, const str* b)
+{
+  return str_diffb(a, b->s, b->len);
+}
+
+int str_diffs(const str* a, const char* b)
+{
+  return str_diffb(a, b, strlen(b));
+}
+
+int str_diffb(const str* a, const char* b, unsigned len)
+{
+  unsigned max;
+  char* aptr;
+  max = a->len;
+  if (max > len) max = len;
+  for (aptr = a->s; len; ++aptr, ++b, --max)
+    if (*b - *aptr) return *b - *aptr;
+  if (a->len > len) return 1;
+  if (a->len < len) return -1;
+  return 0;
+}
