9cbbb1707f067cd7806a6a408e9e25a258649209
Bruce Guenter <bruce@untroubled.org>
2001-07-06 00:17:53 +0000

    Use iopoll instead of poll directly.

--- iobuf/iobuf_timeout.c
+++ iobuf/iobuf_timeout.c
@@ -1,17 +1,17 @@
 #include <errno.h>
-#include <sys/poll.h>
 #include "iobuf.h"
+#include "iopoll.h"
 
 int iobuf_timeout(iobuf* io, int poll_out)
 {
-  struct pollfd pfd;
+  iopoll_fd pfd;
   int result;
   
   if (!io->timeout) return 1;
 
   pfd.fd = io->fd;
-  pfd.events = poll_out ? POLLOUT : POLLIN;
-  while ((result = poll(&pfd, 1, io->timeout)) == -1) {
+  pfd.events = poll_out ? IOPOLL_WRITE : IOPOLL_READ;
+  while ((result = iopoll(&pfd, 1, io->timeout)) == -1) {
     if (errno == EAGAIN) continue;
     IOBUF_SET_ERROR(io);
   }
