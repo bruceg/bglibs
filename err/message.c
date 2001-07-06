1fca9e1728ef889887dfdd5ebc605112951398b3
Bruce Guenter <bruce@untroubled.org>
2001-07-06 00:22:42 +0000

    Increase the maximum number of arguments from 4 to 6, and add the PID to all messages.

--- err/message.c
+++ err/message.c
@@ -1,24 +1,32 @@
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "iobuf/iobuf.h"
 #include "err.h"
 
-static const char colon[] = ": ";
+static pid_t pid = 0;
 
 void err_message(const char* type,
-		 const char* a, const char* b, const char* c, const char* d,
+		 const char* a, const char* b, const char* c,
+		 const char* d, const char* e, const char* f,
 		 int showsys)
 {
+  if (!pid) pid = getpid();
   obuf_puts(&errbuf, program);
-  obuf_puts(&errbuf, colon);
+  obuf_putc(&errbuf, '[');
+  obuf_putu(&errbuf, pid);
+  obuf_puts(&errbuf, "]: ");
   obuf_puts(&errbuf, type);
-  obuf_puts(&errbuf, colon);
+  obuf_puts(&errbuf, ": ");
   if (a) obuf_puts(&errbuf, a);
   if (b) obuf_puts(&errbuf, b);
   if (c) obuf_puts(&errbuf, c);
   if (d) obuf_puts(&errbuf, d);
+  if (e) obuf_puts(&errbuf, e);
+  if (f) obuf_puts(&errbuf, f);
   if (showsys) {
-    obuf_puts(&errbuf, colon);
+    obuf_puts(&errbuf, ": ");
     obuf_puts(&errbuf, strerror(errno));
   }
   obuf_putsflush(&errbuf, "\n");
