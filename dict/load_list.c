0be25083c229b61f60042ec53f14691fec309d1b
Bruce Guenter <bruce@untroubled.org>
2001-08-11 01:55:01 +0000

    Added standard routines for loading up dictionaries.

---
+++ dict/load_list.c
@@ -0,0 +1,21 @@
+#include "iobuf/iobuf.h"
+#include "str/str.h"
+#include "dict.h"
+
+int dict_load_list(dict* d, const char* filename, int mustexist)
+{
+  ibuf in;
+  str tmp = {0,0,0};
+  int result = 1;
+  
+  if (!dict_init(d)) return 0;
+  if (!ibuf_open(&in, filename, 0)) return !mustexist;
+  while (ibuf_getstr(&in, &tmp, '\n')) {
+    str_strip(&tmp);
+    if (tmp.len > 0 && tmp.s[0] != '#')
+      if (!dict_add(d, &tmp, 0)) { result = 0; break; }
+  }
+  str_free(&tmp);
+  ibuf_close(&in);
+  return result;
+}
---
+++ dict/load_map.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include "iobuf/iobuf.h"
+#include "str/str.h"
+#include "dict.h"
+
+int dict_load_map(dict* d, const char* filename, int mustexist, char sep)
+{
+  ibuf in;
+  unsigned i;
+  str tmp = {0,0,0};
+  int result = 1;
+
+  if (!dict_init(d)) return 0;
+  if (!ibuf_open(&in, filename, 0)) return !mustexist;
+  while (ibuf_getstr(&in, &tmp, '\n')) {
+    str_strip(&tmp);
+    if (tmp.len > 0 && tmp.s[0] != '#' &&
+	(i = str_findfirst(&tmp, sep)) != (unsigned)-1) {
+      str* val;
+      if ((val = malloc(sizeof *val)) == 0) { result = 0; break; }
+      memset(val, 0, sizeof *val);
+      if (!str_copyb(val, tmp.s+i+1, tmp.len-i-1) ||
+	  !str_truncate(&tmp, i) ||
+	  !dict_add(d, &tmp, val)) {
+	str_free(val);
+	free(val);
+	result = 0;
+	break;
+      }
+    }
+  }
+  str_free(&tmp);
+  ibuf_close(&in);
+  return result;
+}
