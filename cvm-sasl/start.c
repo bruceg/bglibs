3a838cfab63c0f99b617fcd9a97cdbd93a9ff17a
Bruce Guenter <bruce@untroubled.org>
2001-09-08 00:12:24 +0000

    *** empty log message ***

--- cvm-sasl/start.c
+++ cvm-sasl/start.c
@@ -7,7 +7,7 @@ const char* sasl_mech_cvm = 0;
 
 int sasl_start(const char* mechanism, const str* initresponse, str* challenge)
 {
-  sasl_mechanism* mech;
+  const sasl_mechanism* mech;
   for (mech = sasl_mechanisms; mech != 0; mech = mech->next) {
     if (strcasecmp(mechanism, mech->name) == 0) {
       sasl_mech_cvm = mech->cvm;
