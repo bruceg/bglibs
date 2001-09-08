fbd42f376e24e8330283b6f4321fca9a520ffb73
Bruce Guenter <bruce@untroubled.org>
2001-09-08 00:12:37 +0000

    Fixed operation of the PLAIN mechanism.

--- cvm-sasl/plain.c
+++ cvm-sasl/plain.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "cvm-sasl.h"
 #include "internal.h"
 
@@ -12,10 +13,16 @@ int sasl_plain_response(const str* response, str* challenge)
 {
   const char* creds[2];
   unsigned i;
+  unsigned j;
   if (response->len == 0) return SASL_RESP_BAD;
-  if ((i = str_findfirst(response, 0)) == (unsigned)-1) return SASL_RESP_BAD;
-  creds[0] = response->s+i+1;
+  if ((i = str_findfirst(response, 0)) == (unsigned)-1 ||
+      (j = str_findnext(response, 0, ++i)) == (unsigned)-1)
+    return SASL_RESP_BAD;
+  ++j;
+  if (i != j-i || memcmp(response->s, response->s+i, i) != 0)
+    return SASL_RESP_BAD;
+  creds[0] = response->s+j;
   creds[1] = 0;
-  return sasl_authenticate(response->s, creds);
+  return sasl_authenticate(response->s+i, creds);
   (void)challenge;
 }
