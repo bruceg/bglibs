c7dbb499091583075d3a8da5414437b4c79e1b70
Bruce Guenter <bruce@untroubled.org>
2001-09-15 04:32:11 +0000

    Ignore the first portion of the credential string instead of requiring it to be equal to the second portion.

--- cvm-sasl/plain.c
+++ cvm-sasl/plain.c
@@ -15,12 +15,10 @@ int sasl_plain_response(const str* response, str* challenge)
   unsigned i;
   unsigned j;
   if (response->len == 0) return SASL_RESP_BAD;
-  if ((i = str_findfirst(response, 0)) == (unsigned)-1 ||
-      (j = str_findnext(response, 0, ++i)) == (unsigned)-1)
-    return SASL_RESP_BAD;
+  if ((i = str_findfirst(response, 0)) == (unsigned)-1) return SASL_RESP_BAD;
+  ++i;
+  if ((j = str_findnext(response, 0, i)) == (unsigned)-1) return SASL_RESP_BAD;
   ++j;
-  if (i != j-i || memcmp(response->s, response->s+i, i) != 0)
-    return SASL_RESP_BAD;
   creds[0] = response->s+j;
   creds[1] = 0;
   return sasl_authenticate(response->s+i, creds);
