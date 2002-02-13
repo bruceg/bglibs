f2c012e137a86993a0881a40e00e14ace444bb08
Bruce Guenter <bruce@untroubled.org>
2002-02-13 04:28:40 +0000

    Depricated the use of $CVM_SASL_LOGIN in favour of $CVM_SASL_PLAIN.

--- cvm-sasl/init.c
+++ cvm-sasl/init.c
@@ -3,7 +3,7 @@
 #include "internal.h"
 
 static sasl_mechanism mechanisms[4] = {
-  { "LOGIN",    "CVM_SASL_LOGIN",    0,
+  { "LOGIN",    "CVM_SASL_PLAIN",    0,
     sasl_login_start,    sasl_login_response,    0 },
   { "PLAIN",    "CVM_SASL_PLAIN",    0,
     sasl_plain_start,    sasl_plain_response,    0 },
@@ -16,6 +16,16 @@ static sasl_mechanism mechanisms[4] = {
 const sasl_mechanism* sasl_mechanisms;
 const char* sasl_domain = 0;
 
+static void set_mech(sasl_mechanism* mech, const char* cvm,
+		     sasl_mechanism** prev, sasl_mechanism** first)
+{
+  mech->cvm = cvm;
+  if (*prev != 0)
+    (*prev)->next = mech;
+  else if (*first == 0)
+    *prev = *first = mech;
+}
+
 int sasl_init(void)
 {
   sasl_mechanism* prev;
@@ -23,13 +33,14 @@ int sasl_init(void)
   sasl_mechanism* mech;
   const char* tmp;
   for (mech = mechanisms, first = 0, prev = 0; mech->name != 0; ++mech) {
-    if ((tmp = getenv(mech->var)) != 0) {
-      mech->cvm = tmp;
-      if (prev != 0)
-	prev->next = mech;
-      else if (first == 0)
-	prev = first = mech;
-    }
+    if ((tmp = getenv(mech->var)) != 0)
+      set_mech(mech, tmp, &prev, &first);
+  }
+  /* backwards compatibility for $CVM_SASL_LOGIN */
+  if (!mechanisms[0].cvm && (tmp = getenv("CVM_SASL_LOGIN")) != 0) {
+    prev = 0;
+    first = 0;
+    set_mech(&mechanisms[0], tmp, &prev, &first);
   }
   sasl_mechanisms = first;
   sasl_domain = getenv("TCPLOCALHOST");
