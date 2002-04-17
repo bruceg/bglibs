cb53bfda3ce54b4f1ec93c92c042f507c63de875
Bruce Guenter <bruce@untroubled.org>
2002-04-17 04:55:13 +0000

    Added needsfree flag support.

--- iobuf/iobuf_init.c
+++ iobuf/iobuf_init.c
@@ -7,14 +7,14 @@ unsigned iobuf_bufsize = 4096;
 int iobuf_init(iobuf* io, int fd, unsigned bufsize, char* buffer, unsigned flags)
 {
   memset(io, 0, sizeof *io);
+  if (!bufsize) bufsize = iobuf_bufsize;
+  if (!buffer) {
+    if ((buffer = malloc(bufsize)) == 0) return 0;
+    flags |= IOBUF_NEEDSFREE;
+  }
   io->fd = fd;
   io->buffer = buffer;
   io->bufsize = bufsize;
   io->flags = flags;
-  if (!bufsize) bufsize = iobuf_bufsize;
-  if (!buffer) {
-    if ((buffer = malloc(bufsize)) == 0) return 0;
-    io->flags |= IOBUF_NEEDSFREE;
-  }
   return 1;
 }
