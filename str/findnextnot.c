6846ec2b5e92164b5777aef5332c9c180c00fccf
Bruce Guenter <bruce@untroubled.org>
2002-05-02 23:39:18 +0000

    Added str_find{first,last,next,prev}not functions.

---
+++ str/findnextnot.c
@@ -0,0 +1,30 @@
+/* str/findnextnot.c - Find the next character in a string not of a set
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include "str.h"
+
+int str_findnextnot(const str* s, const char* list, unsigned pos)
+{
+  char* p;
+  int map[256];
+  if (pos >= s->len) return -1;
+  str_buildmap(map, list);
+  for (p = s->s + pos; pos < s->len; pos++, p++)
+    if (map[*(unsigned char*)p] < 0)
+      return pos;
+  return -1;
+}
