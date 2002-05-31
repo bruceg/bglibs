92ab3bd9b47805a58e982968945c0d9e86762935
Bruce Guenter <bruce@untroubled.org>
2002-05-31 05:44:46 +0000

    Remember to free the key string memory.

--- dict/free.c
+++ dict/free.c
@@ -12,8 +12,10 @@ void dict_free(dict* d, void (*free_data)(void*))
       if (table[i] && table[i]->data)
 	free_data(table[i]->data);
   for (i = 0; i < size; i++)
-    if (table[i])
+    if (table[i]) {
+      str_free(&table[i]->key);
       free(table[i]);
-  free(table);
+    }
+  if (table) free(table);
   memset(d, 0, sizeof *d);
 }
