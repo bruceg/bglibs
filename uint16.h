ccdef007956f11f6d382764a59f448fb8eafdd33
Bruce Guenter <bruce@untroubled.org>
2002-12-24 06:25:00 +0000

    Change prototypes to reflect that packed uint16 values are two bytes.

--- uint16.h
+++ uint16.h
@@ -3,11 +3,11 @@
 
 typedef unsigned short uint16;
 
-extern void uint16_pack_lsb(uint16, unsigned char*);
-extern void uint16_pack_msb(uint16, unsigned char*);
-extern void uint16_unpack_lsb(const unsigned char*, uint16*);
-extern void uint16_unpack_msb(const unsigned char*, uint16*);
-#define uint16_pack_lsb uint16_pack
-#define uint16_unpack_lsb uint16_unpack
+extern void uint16_pack_lsb(uint16, unsigned char[2]);
+extern void uint16_pack_msb(uint16, unsigned char[2]);
+extern void uint16_unpack_lsb(const unsigned char[2], uint16*);
+extern void uint16_unpack_msb(const unsigned char[2], uint16*);
+#define uint16_pack uint16_pack_lsb
+#define uint16_unpack uint16_unpack_lsb
 
 #endif
