2bc478bbfd3f6cba64fe898e9c10ed8c90e9dd57
Bruce Guenter <bruce@untroubled.org>
2002-12-27 16:48:51 +0000

    Switched to the standard UINTNN macros.

--- uint16.h
+++ uint16.h
@@ -1,14 +1,8 @@
 #ifndef UINT16_H
 #define UINT16_H
 
-typedef unsigned short uint16;
-
-extern uint16 uint16_get_lsb(const unsigned char[2]);
-extern uint16 uint16_get_msb(const unsigned char[2]);
-extern void uint16_pack_lsb(uint16, unsigned char[2]);
-extern void uint16_pack_msb(uint16, unsigned char[2]);
-extern void uint16_unpack_lsb(const unsigned char[2], uint16*);
-extern void uint16_unpack_msb(const unsigned char[2], uint16*);
+#include <sysdeps.h>
+__UINTNN_DECL(16,2)
 #define uint16_get uint16_get_lsb
 #define uint16_pack uint16_pack_lsb
 #define uint16_unpack uint16_unpack_lsb
