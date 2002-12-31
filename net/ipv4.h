d7ca70320b028fb69cc80ea06dbf82c50db6d075
Bruce Guenter <bruce@untroubled.org>
2002-12-31 04:55:36 +0000

    Prevent name clash with str type.

--- net/ipv4.h
+++ net/ipv4.h
@@ -7,8 +7,8 @@ typedef uint16 ipv4port;
 extern const ipv4addr IPV4ADDR_ANY;
 extern const ipv4addr IPV4ADDR_BROADCAST;
 
-extern int ipv4_parse(const char* str, ipv4addr* addr, const char** end);
-extern const char* ipv4_scan(const char* str, ipv4addr* addr);
+extern int ipv4_parse(const char* s, ipv4addr* addr, const char** end);
+extern const char* ipv4_scan(const char* s, ipv4addr* addr);
 extern const char* ipv4_format(const ipv4addr* addr);
 
 #endif
