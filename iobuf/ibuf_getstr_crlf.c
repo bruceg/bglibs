0cc23ead39744ee511ee11b34054aa1bdce8cab6
Bruce Guenter <bruce@untroubled.org>
2003-01-22 18:07:13 +0000

    Fixed a typo -- need to look at in->count, not ibuf.count. Thanks to Lars Uffmann for the fix.

--- iobuf/ibuf_getstr_crlf.c
+++ iobuf/ibuf_getstr_crlf.c
@@ -6,7 +6,7 @@ int ibuf_getstr_crlf(ibuf* in, struct str* s)
 {
   unsigned len;
   if (!ibuf_getstr(in, s, LF)) return 0;
-  if (inbuf.count == 0) return 0;
+  if (in->count == 0) return 0;
   /* Strip the trailing LF */
   len = s->len-1;
   /* Strip a trailing CRs if present */
