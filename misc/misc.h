f0d80da28dbc7142ca0aec096e8adc967390e4f9
Bruce Guenter <bruce@untroubled.org>
2003-02-11 15:36:58 +0000

    Added the "Mersenne Twister" random number generator MT19937.

--- misc/misc.h
+++ misc/misc.h
@@ -1,6 +1,12 @@
 #ifndef MISC__H__
 #define MISC__H__
 
+#include <sysdeps.h>
+extern void random_init(uint32 seed);
+extern uint32 random_int(void);
+#define random_float() (random_int() * (double)(1.0/4294967296.0))
+#define random_scale(S) ((unsigned int)(random_float() * (S)))
+
 unsigned long strtou(const char* str, const char** end);
 const char* utoa(unsigned long);
 char* utoa2(unsigned long, char*);
