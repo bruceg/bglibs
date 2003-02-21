bf9fcbf6fa42d204f3f6657f493061801546dc0a
Bruce Guenter <bruce@untroubled.org>
2003-02-21 20:40:59 +0000

    Add random_trunc function, which truncates (with integer modulus) instead of scaling (with floating point divide and multiply) the base random number.

--- misc/misc.h
+++ misc/misc.h
@@ -6,6 +6,7 @@ extern void random_init(uint32 seed);
 extern uint32 random_int(void);
 #define random_float() (random_int() * (double)(1.0/4294967296.0))
 #define random_scale(S) ((unsigned int)(random_float() * (S)))
+#define random_trunc(T) (random_int() % (T))
 
 unsigned long strtou(const char* str, const char** end);
 const char* utoa(unsigned long);
