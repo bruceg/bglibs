35cdba285220782b782dac3b4fd240fbea766ac5
Bruce Guenter <bruce@untroubled.org>
2003-03-09 02:45:58 +0000

    Fixed bug in parameters to mmap in iobuf_init.

--- iobuf/iobuf_init.c
+++ iobuf/iobuf_init.c
@@ -18,7 +18,7 @@ int iobuf_init(iobuf* io, int fd, unsigned bufsize, char* buffer, unsigned flags
   if (!bufsize) bufsize = iobuf_bufsize;
   if (!buffer) {
     if ((buffer = mmap(0, bufsize, PROT_READ|PROT_WRITE,
-		       MAP_PRIVATE|MAP_ANONYMOUS, 0, 0)) != 0)
+		       MAP_PRIVATE|MAP_ANONYMOUS, -1, 0)) != MAP_FAILED)
       flags |= IOBUF_NEEDSMUNMAP;
     else {
       if ((buffer = malloc(bufsize)) == 0) return 0;
