7a85382b76be0a827a66e031721876fa91dc645e
Bruce Guenter <bruce@untroubled.org>
2003-03-14 20:07:27 +0000

    Switched the 64-bit CRC code from a "reversed" to a "normal" algorithm.

--- crc/crc64_update.c
+++ crc/crc64_update.c
@@ -4,6 +4,6 @@ uint64 crc64_update(uint64 crc, const char* data, long len)
 {
   const unsigned char* ptr = data;
   while (len-- > 0)
-    crc = crc64_table[(crc ^ *ptr++) & 0xff] ^ (crc >> 8);
+    crc = crc64_table[(unsigned char)(crc>>56) ^ *ptr++] ^ (crc << 8);
   return crc;
 }
