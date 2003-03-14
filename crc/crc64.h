ab788b0e87f22583eb8b6994c9ace69d751f989e
Bruce Guenter <bruce@untroubled.org>
2003-03-14 20:04:32 +0000

    Switched the polynomial to something more useful looking, first seen in the PostgreSQL code, taken in turn from the DLT1 spec (ECMA-182).

--- crc/crc64.h
+++ crc/crc64.h
@@ -3,9 +3,15 @@
 
 #include <uint64.h>
 
-/* Polynomial: x^64 + x^4 + x^3 + x^1 + 1 */
-#define CRC64REVPOLY ((uint64)0xd800000000000000ULL)
+/* Polynomial: x^64 + x^62 + x^57 + x^55 + x^54 + x^53 + x^52 + x^47 +
+ * x^46 + x^45 + x^40 + x^39 + x^38 + x^37 + x^35 + x^33 + x^32 + x^31 +
+ * x^29 + x^27 + x^24 + x^23 + x^22 + x^21 + x^19 + x^17 + x^13 + x^12 +
+ * x^10 + x^9 + x^7 + x^4 + x + 1
+ * Borrowed from the DLT1 spec (ECMA-182).
+ */
+#define CRC64POLY    ((uint64)0x42F0E1EBA9EA3693ULL)
 #define CRC64INIT    ((uint64)0xffffffffffffffffULL)
+#define CRC64POST    ((uint64)0xffffffffffffffffULL)
 
 extern const uint64 crc64_table[256];
 extern uint64 crc64_update(uint64 crc, const char* bytes, long len);
