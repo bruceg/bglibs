97c8edd65a3e934b3b1c0f4a9ed4e90748a5decb
Bruce Guenter <bruce@untroubled.org>
2003-03-18 16:57:57 +0000

    Started building up self-test framework.

---
+++ selftest.c
@@ -0,0 +1,40 @@
+#include <string.h>
+
+#include "iobuf/iobuf.h"
+#include "msg/msg.h"
+#include "str/str.h"
+
+void NL(void) { obuf_putc(&outbuf, LF); }
+
+void debugstr(const str* s)
+{
+  obuf_puts(&outbuf, "len=");
+  obuf_putu(&outbuf, s->len);
+  obuf_puts(&outbuf, " size=");
+  obuf_putu(&outbuf, s->size);
+  if (s->s == 0)
+    obuf_puts(&outbuf, " s is NULL");
+  else {
+    obuf_puts(&outbuf, " s=");
+    obuf_putstr(&outbuf, s);
+  }
+  NL();
+}
+
+void debugstrfn(int result, const str* s)
+{
+  obuf_puts(&outbuf, "result=");
+  obuf_puti(&outbuf, result);
+  obuf_putc(&outbuf, ' ');
+  debugstr(s);
+}
+
+#define MAIN void selftest(void)
+MAIN;
+
+int main(void)
+{
+  selftest();
+  obuf_flush(&outbuf);
+  return 0;
+}
---
+++ selftests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+exitcode=true
+t=$0.tmp.$$
+mkdir -v $t
+trap 'rm -r $t' EXIT
+fgrep -l '#ifdef SELFTEST_MAIN' */*.c | while read c
+do
+  echo "Testing $c"
+  sed -e '1,/^#ifdef SELFTEST_EXP$/d' -e '/^#endif/,$d' "$c" >$t/test.exp
+  if ! ./compile "$c" -DSELFTEST_MAIN -o $t/test.o
+  then
+    echo "=====> Compile failed! <====="
+    exitcode=false
+  elif ! ./load $t/test */lib.a
+  then
+    echo "=====> Load failed! <====="
+    exitcode=false
+  elif ! $t/test >$t/test.out 2>&1
+  then
+    echo "=====> Test failed! <====="
+    exitcode=false
+  elif ! diff -a -U 9999 $t/test.exp $t/test.out
+  then
+    echo "=====> Output failed! <====="
+    exitcode=false
+  fi
+done
+$exitcode
