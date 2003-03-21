94f0ba071ff79955e342c854111897c0e7d8e215
Bruce Guenter <bruce@untroubled.org>
2003-03-21 20:03:54 +0000

    Added self-tests, which showed up several bugs.

--- path/fnmatch.c
+++ path/fnmatch.c
@@ -25,6 +25,11 @@
  * [~seq] matches any single character not in seq
  */
 
+static int is_magic(char c)
+{
+  return c == '*' || c == '?' || c == '[';
+}
+
 int has_magic(const char* s)
 {
   return strchr(s, '*') || strchr(s, '?') || strchr(s, '[');
@@ -43,7 +48,9 @@ static int fnmatchr1(const char* filename, const char* pattern)
 {
   const char* end;
   const char* start;
-  for (; *pattern && *filename; ++pattern, ++filename) {
+  for (; *pattern; ++pattern, ++filename) {
+    if (*filename == 0)
+      return pattern[0] == '*' && pattern[1] == 0;
     switch (*pattern) {
     case '?': continue;
     case '*':
@@ -64,10 +71,18 @@ static int fnmatchr1(const char* filename, const char* pattern)
 static int fnmatchr2(const char* name, const char* pattern)
 {
   char start = *pattern;
+  int r;
   if (start == 0) return 1;
-  while (*name) {
-    if (*name == start && fnmatchr1(name+1, pattern+1)) return 1;
-    ++name;
+  if (is_magic(start)) {
+    for (; *name != 0; ++name)
+      if ((r = fnmatchr1(name, pattern)) != 0)
+	return r;
+  }
+  else {
+    ++pattern;
+    while (*name)
+      if (*name++ == start && (r = fnmatchr1(name, pattern)) != 0)
+	return r;
   }
   return 0;
 }
@@ -86,3 +101,159 @@ int fnmatch(const char* filename, const char* pattern, unsigned options)
   }
   return fnmatchr1(filename, pattern);
 }
+
+#ifdef SELFTEST_MAIN
+#include "selftest.c"
+void t(const char* pattern, unsigned options)
+{
+  obuf_puts(&outbuf, pattern);
+  obuf_putc(&outbuf, ' ');
+  obuf_putu(&outbuf, options);
+  obuf_putc(&outbuf, ' ');
+  debugfn(fnmatch("abc", pattern, options));
+}
+MAIN
+{
+  t("*", 0);
+  t("**", 0);
+  /* Three patterns for each case:
+   * 1 at start of filename
+   * 2 in filename
+   * 3 at end of filename
+   * Four characters to test:
+   * 1 First filename character "a"
+   * 2 Second filename character "b"
+   * 3 Third filename character "c"
+   * 4 Non-filename character "x"
+   */
+  /* First case: single character */
+  t("a*", 0);
+  t("*a*", 0);
+  t("*a", 0);
+  t("b*", 0);
+  t("*b*", 0);
+  t("*b", 0);
+  t("c*", 0);
+  t("*c*", 0);
+  t("*c", 0);
+  t("x*", 0);
+  t("*x*", 0);
+  t("*x", 0);
+  /* Second case: character in a set */
+  t("[axy]*", 0);
+  t("*[axy]*", 0);
+  t("*[axy]", 0);
+  t("[bxy]*", 0);
+  t("*[bxy]*", 0);
+  t("*[bxy]", 0);
+  t("[cxy]*", 0);
+  t("*[cxy]*", 0);
+  t("*[cxy]", 0);
+  t("[xyz]*", 0);
+  t("*[xyz]*", 0);
+  t("*[xyz]", 0);
+  /* Third case: negated set */
+  t("[~a]*", 0);
+  t("*[~a]*", 0);
+  t("*[~a]", 0);
+  t("[~b]*", 0);
+  t("*[~b]*", 0);
+  t("*[~b]", 0);
+  t("[~c]*", 0);
+  t("*[~c]*", 0);
+  t("*[~c]", 0);
+  t("[~x]*", 0);
+  t("*[~x]*", 0);
+  t("*[~x]", 0);
+  /* Fourth case: preceding wildcard */
+  t("?a*", 0);
+  t("*?a*", 0);
+  t("*?a", 0);
+  t("?b*", 0);
+  t("*?b*", 0);
+  t("*?b", 0);
+  t("?c*", 0);
+  t("*?c*", 0);
+  t("*?c", 0);
+  t("?x*", 0);
+  t("*?x*", 0);
+  t("*?x", 0);
+  /* Fifth case: following wildcard */
+  t("a?*", 0);
+  t("*a?*", 0);
+  t("*a?", 0);
+  t("b?*", 0);
+  t("*b?*", 0);
+  t("*b?", 0);
+  t("c?*", 0);
+  t("*c?*", 0);
+  t("*c?", 0);
+  t("x?*", 0);
+  t("*x?*", 0);
+  t("*x?", 0);
+}
+#endif
+#ifdef SELFTEST_EXP
+* 0 result=1
+** 0 result=1
+a* 0 result=1
+*a* 0 result=1
+*a 0 result=0
+b* 0 result=0
+*b* 0 result=1
+*b 0 result=0
+c* 0 result=0
+*c* 0 result=1
+*c 0 result=1
+x* 0 result=0
+*x* 0 result=0
+*x 0 result=0
+[axy]* 0 result=1
+*[axy]* 0 result=1
+*[axy] 0 result=0
+[bxy]* 0 result=0
+*[bxy]* 0 result=1
+*[bxy] 0 result=0
+[cxy]* 0 result=0
+*[cxy]* 0 result=1
+*[cxy] 0 result=1
+[xyz]* 0 result=0
+*[xyz]* 0 result=0
+*[xyz] 0 result=0
+[~a]* 0 result=0
+*[~a]* 0 result=1
+*[~a] 0 result=1
+[~b]* 0 result=1
+*[~b]* 0 result=1
+*[~b] 0 result=1
+[~c]* 0 result=1
+*[~c]* 0 result=1
+*[~c] 0 result=0
+[~x]* 0 result=1
+*[~x]* 0 result=1
+*[~x] 0 result=1
+?a* 0 result=0
+*?a* 0 result=0
+*?a 0 result=0
+?b* 0 result=1
+*?b* 0 result=1
+*?b 0 result=0
+?c* 0 result=0
+*?c* 0 result=1
+*?c 0 result=1
+?x* 0 result=0
+*?x* 0 result=0
+*?x 0 result=0
+a?* 0 result=1
+*a?* 0 result=1
+*a? 0 result=0
+b?* 0 result=0
+*b?* 0 result=1
+*b? 0 result=1
+c?* 0 result=0
+*c?* 0 result=0
+*c? 0 result=0
+x?* 0 result=0
+*x?* 0 result=0
+*x? 0 result=0
+#endif
