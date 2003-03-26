16b5f11add7fcf51a83f50bd69b4ffa5f2e6b18c
Bruce Guenter <bruce@untroubled.org>
2003-03-26 17:17:03 +0000

    Modified to allow running individual tests.

--- selftests.sh
+++ selftests.sh
@@ -12,44 +12,57 @@ echo "Creating temporary directory $t"
 mkdir $t
 trap 'echo "Cleaning up $t"; rm -r $t' EXIT
 
-exitcode=true
-for c in `fgrep -l '#ifdef SELFTEST_MAIN' */*.c`
-do
+do_test() {
+  local c="$1"
   echo "Testing $c"
+  rm -f $t/*
   sed -e '1,/^#ifdef SELFTEST_EXP$/d' -e '/^#endif/,$d' $c >$t/test.exp
 
   ./compile $c -DSELFTEST_MAIN -o $t/test.o
   if [ $? -ne 0 ]
   then
     echo "=====> Compile failed! <====="
-    exitcode=false
-    continue
+    return 1
   fi
 
   ./load $t/test */lib.a libbg-sysdeps.a
   if [ $? -ne 0 ]
   then
     echo "=====> Load failed! <====="
-    exitcode=false
-    continue
+    return 1
   fi
 
   ( cd $t && ./test >./test.out 2>&1; )
   if [ $? -ne 0 ]
   then
     echo "=====> Test failed! <====="
-    exitcode=false
-    continue
+    return 1
   fi
 
   $DIFF $t/test.exp $t/test.out
   if [ $? -ne 0 ]
   then
     echo "=====> Output failed! <====="
-    exitcode=false
-    continue
+    return 1
   fi
+  return 0
+}
 
-  rm -f $t/*
-done
+exitcode=true
+if [ $# -gt 0 ]; then
+  for c in "$@"; do
+    do_test $c
+    if [ $? -ne 0 ]; then
+      exitcode=false;
+    fi
+  done
+else
+  for c in `fgrep -l '#ifdef SELFTEST_MAIN' */*.c`
+  do
+    do_test $c
+    if [ $? -ne 0 ]; then
+      exitcode=false;
+    fi
+  done
+fi
 $exitcode
