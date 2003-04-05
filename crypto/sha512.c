a299b1fb65705c50c744c945b251aa066ce4c077
Bruce Guenter <bruce@untroubled.org>
2003-04-05 04:47:46 +0000

    Fixed a harmless warning, and added several more test vectors from http://www.aarongifford.com/computers/sha.html

--- crypto/sha512.c
+++ crypto/sha512.c
@@ -335,7 +335,9 @@ void SHA512_update(SHA512_ctx* ctx, const void* vdata, unsigned long data_len)
    
    /* convert data_len to bits and add to the 128 bit word formed by lbits
       and hbits */
+#ifdef HAS_ULONG64
    ctx->hbits += data_len >> 61;
+#endif
    low_bits = data_len << 3;
    ctx->lbits += low_bits;
    if (ctx->lbits < low_bits) ++ctx->hbits;
@@ -399,10 +401,24 @@ int main(void)
 {
   test("abc");
   test("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+  /* Additional test vectors found in
+   * http://www.aarongifford.com/computers/sha.html */
+  test("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+  test("");
+  test("This is exactly 64 bytes long, not counting the terminating byte");
+  test("For this sample, this 63-byte string will be used as input data");
+  test("And this textual data, astonishing as it may appear, is exactly 128 bytes in length, as are both SHA-384 and SHA-512 block sizes");
+  test("By hashing data that is one byte less than a multiple of a hash block length (like this 127-byte string), bugs may be revealed.");
   return 0;
 }
 #endif
 #ifdef SELFTEST_EXP
 ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f
 8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909
+204a8fc6dda82f0a0ced7beb8e08a41657c16ef468b228a8279be331a703c33596fd15c13b1b07f9aa1d3bea57789ca031ad85c7a71dd70354ec631238ca3445
+cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
+70aefeaa0e7ac4f8fe17532d7185a289bee3b428d950c14fa8b713ca09814a387d245870e007a80ad97c369d193e41701aa07f3221d15f0e65a1ff970cedf030
+b3de4afbc516d2478fe9b518d063bda6c8dd65fc38402dd81d1eb7364e72fb6e6663cf6d2771c8f5a6da09601712fb3d2a36c6ffea3e28b0818b05b0a8660766
+97fb4ec472f3cb698b9c3c12a12768483e5b62bcdad934280750b4fa4701e5e0550a80bb0828342c19631ba55a55e1cee5de2fda91fc5d40e7bee1d4e6d415b3
+d399507bbf5f2d0da51db1ff1fc51c1c9ff1de0937e00d01693b240e84fcc3400601429f45c297acc6e8fcf1e4e4abe9ff21a54a0d3d88888f298971bd206cd5
 #endif
