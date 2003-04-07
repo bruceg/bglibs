73db245a30f56eb0645165413ed5f32f0017a516
Bruce Guenter <bruce@untroubled.org>
2003-04-07 15:24:05 +0000

    Rewrote the SHA-256 algorithm from scratch, based on the SHA-512 code.

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,252 +1,212 @@
-/*
- * Implementation of SHA-256, based on Adam Back's sha-1 implementation.
- * Based on the implementation that was posted anonymously to
- * coderpunks@toad.com on October 12th, 2000.
- * Believed to be public domain.
+/* sha256.c - SHA-256 algorithm
+ * Copyright (C) 2003  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <string.h>
 #include "sha256.h"
+#include "uint32.h"
+#include "uint64.h"
 
-#define min( x, y ) ( ( x ) < ( y ) ? ( x ) : ( y ) )
+#define S(x,n) (((x)>>n) | ((x)<<(32-n)))
+#define R(x,n) ((x)>>n)
+#define Ch(x,y,z) (((x)&(y)) ^ (~(x)&(z)))
+#define Maj(x,y,z) (((x)&(y)) ^ ((x)&(z)) ^ ((y)&(z)))
+#define S0(x) (S(x, 2) ^ S(x,13) ^ S(x,22))
+#define S1(x) (S(x, 6) ^ S(x,11) ^ S(x,25))
+#define s0(x) (S(x, 7) ^ S(x,18) ^ R(x, 3))
+#define s1(x) (S(x,17) ^ S(x,19) ^ R(x,10))
+#define F(a,b,c,d,e,f,g,h) \
+    T1 = h + S1(e) + Ch(e,f,g) + *Kp++ + *Wp++; \
+    T2 = S0(a) + Maj(a,b,c); \
+    d += T1; \
+    h = T1 + T2; \
 
-#define S(x,n) ( ((x)>>(n)) | ((x)<<(32-(n))) )
-#define R(x,n) ( (x)>>(n) )
-
-#define Ch(x,y,z)  ( ((x) & (y)) | (~(x) & (z)) )
-#define Maj(x,y,z) ( ( (x) & (y) ) | ( (x) & (z) ) | ( (y) & (z) ) )
-
-#define SIG0(x) ( S(x, 2) ^ S(x,13) ^ S(x,22) )
-#define SIG1(x) ( S(x, 6) ^ S(x,11) ^ S(x,25) )
-#define sig0(x) ( S(x, 7) ^ S(x,18) ^ R(x, 3) )
-#define sig1(x) ( S(x,17) ^ S(x,19) ^ R(x,10) )
-
-static const uint32 K[] = {
-	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-	0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-	0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+static const uint32 K[64] = {
+  0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL,
+  0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
+  0xd807aa98UL, 0x12835b01UL, 0x243185beUL, 0x550c7dc3UL,
+  0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL, 0xc19bf174UL,
+  0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL,
+  0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL,
+  0x983e5152UL, 0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL,
+  0xc6e00bf3UL, 0xd5a79147UL, 0x06ca6351UL, 0x14292967UL,
+  0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL, 0x53380d13UL,
+  0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
+  0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL,
+  0xd192e819UL, 0xd6990624UL, 0xf40e3585UL, 0x106aa070UL,
+  0x19a4c116UL, 0x1e376c08UL, 0x2748774cUL, 0x34b0bcb5UL,
+  0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL, 0x682e6ff3UL,
+  0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL,
+  0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2UL
 };
 
+static const uint32 H[8] = {
+  0x6a09e667UL,
+  0xbb67ae85UL,
+  0x3c6ef372UL,
+  0xa54ff53aUL,
+  0x510e527fUL,
+  0x9b05688cUL,
+  0x1f83d9abUL,
+  0x5be0cd19UL
+};
 
-#define H1	0x6a09e667
-#define H2	0xbb67ae85
-#define H3	0x3c6ef372
-#define H4	0xa54ff53a
-#define H5	0x510e527f
-#define H6	0x9b05688c
-#define H7	0x1f83d9ab
-#define H8	0x5be0cd19
-
-uint32 INIT[ 8 ] = { H1, H2, H3, H4, H5, H6, H7, H8 };
-
-
-/* convert to big endian where needed */
-
-static void convert_to_bigendian( void* data, int len )
+static void SHA256_transform(SHA256_ctx* ctx)
 {
-/* test endianness */
-   uint32 test_value = 0x01;
-   uint8* test_as_bytes = (uint8*) &test_value;
-   int little_endian = test_as_bytes[ 0 ];
+  unsigned j;
+  uint32 a = ctx->H[0];
+  uint32 b = ctx->H[1];
+  uint32 c = ctx->H[2];
+  uint32 d = ctx->H[3];
+  uint32 e = ctx->H[4];
+  uint32 f = ctx->H[5];
+  uint32 g = ctx->H[6];
+  uint32 h = ctx->H[7];
+  uint32 T1;
+  uint32 T2;
+  uint32 W[64];
+  uint32* Wp;
+  const uint32* Kp;
 
-   uint32 temp;
-   uint8* temp_as_bytes = (uint8*) &temp;
-   uint32* data_as_words = (uint32*) data;
-   uint8* data_as_bytes;
-   int i;
+  for (j = 0; j < 16; ++j)
+    W[j] = uint32_get_msb(ctx->M + j*4);
+  
+  for (j = 16; j < 64; ++j)
+    W[j] = s1(W[j-2]) + W[j-7] + s0(W[j-15]) + W[j-16];
 
-   if ( little_endian )
-   {
-      len /= 4;
-      for ( i = 0; i < len; i++ )
-      {
-	 temp = data_as_words[ i ];
-	 data_as_bytes = (uint8*) &( data_as_words[ i ] );
-	 
-	 data_as_bytes[ 0 ] = temp_as_bytes[ 3 ];
-	 data_as_bytes[ 1 ] = temp_as_bytes[ 2 ];
-	 data_as_bytes[ 2 ] = temp_as_bytes[ 1 ];
-	 data_as_bytes[ 3 ] = temp_as_bytes[ 0 ];
-      }
-   }
-
-/* on big endian machines do nothing as the CPU representation
-   automatically does the right thing for SHA1 */
-}
-
-void SHA256_init( SHA256_ctx* ctx )
-{
-   memcpy( ctx->H, INIT, 8 * sizeof( uint32 ) );
-   ctx->lbits = 0;
-   ctx->hbits = 0;
-   ctx->mlen = 0;
-}
-
-static void SHA256_transform( SHA256_ctx* ctx )
-{
-   int t;
-   uint32 A = ctx->H[ 0 ];
-   uint32 B = ctx->H[ 1 ];
-   uint32 C = ctx->H[ 2 ];
-   uint32 D = ctx->H[ 3 ];
-   uint32 E = ctx->H[ 4 ];
-   uint32 F = ctx->H[ 5 ];
-   uint32 G = ctx->H[ 6 ];
-   uint32 H = ctx->H[ 7 ];
-   uint32 T1, T2;
-   uint32 W[ 64 ];
-
-   memcpy( W, ctx->M, 64 );
-
-   for ( t = 16; t < 64; t++ )
-   {
-      W[ t ] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
-   }
-
-   for ( t = 0; t < 64; t++ )
-   {
-
-#ifdef DEBUG_SHA256
-      printf ("i = %2d  ", t);
-      printf ("%08x %08x %08x %08x %08x %08x %08x %08x\n", A,B,C,D,E,F,G,H);
+#ifdef SHA2_NO_UNROLL
+  for (Wp = W, Kp = K, j = 0; j < 64; ++j) {
+    /* Straight out of the standards document. */
+    T1 = h + S1(e) + Ch(e,f,g) + *Kp++ + *Wp++;
+    T2 = S0(a) + Maj(a,b,c);
+    h = g;
+    g = f;
+    f = e;
+    e = d + T1;
+    d = c;
+    c = b;
+    b = a;
+    a = T1 + T2;
+  }
+#else
+  for (Wp = W, Kp = K, j = 0; j < 8; ++j) {
+    /* Unrolled loop, eliminates most of the extraneous data copies. */
+    F(a,b,c,d,e,f,g,h);
+    F(h,a,b,c,d,e,f,g);
+    F(g,h,a,b,c,d,e,f);
+    F(f,g,h,a,b,c,d,e);
+    F(e,f,g,h,a,b,c,d);
+    F(d,e,f,g,h,a,b,c);
+    F(c,d,e,f,g,h,a,b);
+    F(b,c,d,e,f,g,h,a);
+  }
 #endif
-      T1 = H + SIG1(E) + Ch(E,F,G) + K[t] + W[t]; 
-      T2 = SIG0(A) + Maj(A,B,C);
-      H = G;
-      G = F;
-      F = E;
-      E = D + T1;
-      D = C;
-      C = B;
-      B = A;
-      A = T1 + T2;
-   }
-
-#ifdef DEBUG_SHA256
-   printf ("i = %2d  ", t);
-   printf ("%08x %08x %08x %08x %08x %08x %08x %08x\n", A,B,C,D,E,F,G,H);
-#endif
-   ctx->H[ 0 ] += A;
-   ctx->H[ 1 ] += B;
-   ctx->H[ 2 ] += C;
-   ctx->H[ 3 ] += D;
-   ctx->H[ 4 ] += E;
-   ctx->H[ 5 ] += F;
-   ctx->H[ 6 ] += G;
-   ctx->H[ 7 ] += H;
+  
+  ctx->H[0] += a;
+  ctx->H[1] += b;
+  ctx->H[2] += c;
+  ctx->H[3] += d;
+  ctx->H[4] += e;
+  ctx->H[5] += f;
+  ctx->H[6] += g;
+  ctx->H[7] += h;
 }
 
-void SHA256_update( SHA256_ctx* ctx, const void* vdata, unsigned long bytes )
+void SHA256_init(SHA256_ctx* ctx)
 {
-   const uint8* data = vdata;
-   uint32 use;
-   uint32 low_bits;
-   
-/* convert bytes to bits and add to the 64 bit word formed by lbits
-   and hbits */
-
-   ctx->hbits += bytes >> 29;
-   low_bits = bytes << 3;
-   ctx->lbits += low_bits;
-   if ( ctx->lbits < low_bits ) { ctx->hbits++; }
-
-/* deal with first block */
-
-   if ((use = 64 - ctx->mlen) > bytes) use = bytes;
-   memcpy( ctx->M + ctx->mlen, data, use );
-   ctx->mlen += use;
-   bytes -= use;
-   data += use;
-
-   while ( ctx->mlen == 64 )
-   {
-      convert_to_bigendian( (uint32*)ctx->M, 64 );
-      SHA256_transform( ctx );
-      if ((use = bytes) > 64) use = 64;
-      memcpy( ctx->M, data, use );
-      ctx->mlen = use;
-      bytes -= use;
-      data += use;		/* was missing */
-   }
+  memcpy(ctx->H, H, sizeof H);
+  ctx->bits = 0;
+  ctx->mlen = 0;
 }
 
-void SHA256_final( SHA256_ctx* ctx )
+void SHA256_update(SHA256_ctx* ctx, const void* vdata, unsigned long data_len)
 {
-   if ( ctx->mlen < 56 )
-   {
-      ctx->M[ ctx->mlen ] = 0x80; ctx->mlen++;
-      memset( ctx->M + ctx->mlen, 0x00, 56 - ctx->mlen );
-      convert_to_bigendian( ctx->M, 56 );
-   }
-   else
-   {
-      ctx->M[ ctx->mlen ] = 0x80; 
-      ctx->mlen++;
-      memset( ctx->M + ctx->mlen, 0x00, 64 - ctx->mlen );
-      convert_to_bigendian( ctx->M, 64 );
-      SHA256_transform( ctx );
-      memset( ctx->M, 0x00, 56 );
-   }
+  const uint8* data = vdata;
+  unsigned long use;
 
-   memcpy( ctx->M + 56, (void*)(&(ctx->hbits)), 8 );
-   SHA256_transform( ctx );
+  ctx->bits += data_len << 3;
+
+  while (data_len >= (use = 64 - ctx->mlen)) {
+    memcpy(ctx->M + ctx->mlen, data, use);
+    SHA256_transform(ctx);
+    ctx->mlen = 0;
+    data_len -= use;
+    data += use;
+  }
+  memcpy(ctx->M + ctx->mlen, data, data_len);
+  ctx->mlen += data_len;
 }
 
-void SHA256_digest( SHA256_ctx* ctx, uint8* digest )
+void SHA256_final(SHA256_ctx* ctx, uint8* digest)
 {
-   if ( digest )
-   {
-      memcpy( digest, ctx->H, 8 * sizeof( uint32 ) );
-      convert_to_bigendian( digest, 8 * sizeof( uint32 ) );
-   }
+  unsigned i;
+  ctx->M[ctx->mlen++] = 0x80;
+  memset(ctx->M + ctx->mlen, 0x00, 64 - ctx->mlen);
+  if (ctx->mlen >= 64-8) {
+    SHA256_transform(ctx);
+    memset(ctx->M, 0x00, 64-8);
+  }
+
+  uint64_pack_msb(ctx->bits, ctx->M+64-8);
+  SHA256_transform(ctx);
+
+  for (i = 0; i < 32/4; ++i, digest += 4)
+    uint32_pack_msb(ctx->H[i], digest);
+  memset(ctx, 0, sizeof *ctx);
 }
 
 #ifdef SELFTEST_MAIN
 #include <stdio.h>
 
-static void test(const char* str, long len)
+static void test(const char* str)
 {
-   SHA256_ctx ctx;
-   unsigned i;
-   unsigned char digest[ SHA256_DIGEST_LENGTH ];
-   SHA256_init( &ctx );
-   SHA256_update( &ctx, str, len );
-   SHA256_final( &ctx );
-   SHA256_digest( &ctx, digest );
-   for ( i = 0; i < sizeof(digest) ; i++ )
-      printf( "%02x", digest[ i ] );
-   printf( "\n" );
-   for ( i = 0; i < 8; i++ )
-      printf( "%08lx", (unsigned long)ctx.H[ i ] );
-   printf( "\n" );
+  SHA256_ctx ctx;
+  unsigned i;
+  unsigned char digest[SHA256_DIGEST_LENGTH];
+  SHA256_init(&ctx);
+  SHA256_update(&ctx, str, strlen(str));
+  SHA256_final(&ctx, digest);
+  for (i = 0; i < sizeof(digest); i++)
+    printf("%02x", digest[i]);
+  printf("\n");
 }
 
 int main( void )
 {
-   const char test1[] = "abc";
-   const char test2[] = 
-      "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
-   test( test1, strlen( test1 ) );
-   test( test2, strlen( test2 ) );
-   return 0;
+  test("abc");
+  test("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+  /* Additional test vectors found in
+   * http://www.aarongifford.com/computers/sha.html */
+  test("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+  test("");
+  test("This is exactly 64 bytes long, not counting the terminating byte");
+  test("For this sample, this 63-byte string will be used as input data");
+  test("And this textual data, astonishing as it may appear, is exactly 128 bytes in length, as are both SHA-384 and SHA-512 block sizes");
+  test("By hashing data that is one byte less than a multiple of a hash block length (like this 127-byte string), bugs may be revealed.");
+  return 0;
 }
 #endif
 #ifdef SELFTEST_EXP
 ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
-ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
 248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1
-248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1
+cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+ab64eff7e88e2e46165e29f2bce41826bd4c7b3552f6b382a9e7d3af47c245f8
+f08a78cbbaee082b052ae0708f32fa1e50c5c421aa772ba5dbb406a2ea6be342
+0ab803344830f92089494fb635ad00d76164ad6e57012b237722df0d7ad26896
+e4326d0459653d7d3514674d713e74dc3df11ed4d30b4013fd327fdb9e394c26
 #endif
--- crypto/sha256.h
+++ crypto/sha256.h
@@ -1,20 +1,19 @@
-#if !defined( _sha256_h )
-#define _sha256_h
+#ifndef BGLIBS__SHA256__H__
+#define BGLIBS__SHA256__H__
 
 #include <sysdeps.h>
 
 #define SHA256_DIGEST_LENGTH (256/8)
 
 typedef struct {
-      uint32 H[ 8 ];
-      uint32 hbits, lbits;
-      uint8 M[ 64 ];
-      uint8 mlen;
+  uint32 H[8];
+  uint64 bits;
+  uint8 M[64];
+  unsigned mlen;
 } SHA256_ctx;
 
-void SHA256_init  ( SHA256_ctx* );
-void SHA256_update( SHA256_ctx*, const void*, unsigned long );
-void SHA256_final ( SHA256_ctx* );
-void SHA256_digest( SHA256_ctx*, uint8* );
+void SHA256_init(SHA256_ctx*);
+void SHA256_update(SHA256_ctx*, const void*, unsigned long);
+void SHA256_final(SHA256_ctx*, uint8*);
 
 #endif
