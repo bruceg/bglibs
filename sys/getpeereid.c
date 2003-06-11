f18a454fb91e72fd10f01cd98a1747664a08cc21
Bruce Guenter <bruce@untroubled.org>
2003-06-11 17:16:55 +0000

    Added getpeereid sysdep function.

---
+++ sys/getpeereid.c
@@ -0,0 +1,29 @@
+#include "sysdeps.h"
+
+#if ! HASGETPEEREID
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <unistd.h>
+
+int getpeereid(int s, uid_t* u, gid_t* g)
+{
+#if HASPEERCRED
+  struct ucred peer;
+  int optlen = sizeof peer;
+  if (getsockopt(s, SOL_SOCKET, SO_PEERCRED, &peer, &optlen) == -1)
+    return -1;
+  if (u != 0)
+    *u = peer.uid;
+  if (g != 0)
+    *g = peer.gid;
+  return 0;
+#else
+  errno = ENOSYS;
+  return -1;
+  s = 0; u = 0; g = 0;
+#endif
+}
+
+#endif /* HASGETPEEREID */
