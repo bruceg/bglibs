264185407d1b2d902cd410ce5256cf42c93a4865
Bruce Guenter <bruce@untroubled.org>
2003-06-11 18:04:16 +0000

    Use a modified "compile" script to compile selftests, as some versions of gcc (and maybe other cc's) don't like multiple "-o" options.

--- selftests.sh
+++ selftests.sh
@@ -14,7 +14,8 @@ do_test() {
   rm -f $t/*
   sed -e '1,/^#ifdef SELFTEST_EXP$/d' -e '/^#endif/,$d' $1 >$t/test.exp
 
-  ./compile $1 -DSELFTEST_MAIN -o $t/test.o || {
+  sed -e 's/-o ${base}.o//' compile | \
+  sh -s $1 -DSELFTEST_MAIN -o $t/test.o || {
     echo "=====> Compile failed! <====="
     return 1
   }
