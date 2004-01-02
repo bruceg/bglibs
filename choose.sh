84865e6fd76afc0d11bcfff30adb75291d5c0328
Bruce Guenter <bruce@untroubled.org>
2004-01-02 17:17:12 +0000

    Simplified the choose script by one more step.

--- choose.sh
+++ choose.sh
@@ -1,18 +1,17 @@
-try="sys/$2"
-h1="sys/$3.h1"
-h2="sys/$3.h2"
-result="$h2"
+try="sys/try$2"
+h="sys/${3:-has$2}.h"
+result="${h}2"
 
 case "$1" in
-  *c*) ./compile "$try".c >/dev/null 2>&1 || result="$h1" ;;
+  *c*) ./compile "$try".c >/dev/null 2>&1 || result="${h}1" ;;
 esac
 
 case "$1" in
-  *l*) ./load "$try" >/dev/null 2>&1 || result="$h1" ;;
+  *l*) ./load "$try" >/dev/null 2>&1 || result="${h}1" ;;
 esac
 
 case "$1" in
-  *r*) ./"$try" >/dev/null 2>&1 || result="$h1" ;;
+  *r*) ./"$try" >/dev/null 2>&1 || result="${h}1" ;;
 esac
 
 rm -f "$try".o "$try"
