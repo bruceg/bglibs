5cc830952e610603e92812f37ab75d16808f5664
Bruce Guenter <bruce@untroubled.org>
2004-01-30 18:22:15 +0000

    Added socket connect functions with a timeout.

---
+++ net/connect4_timeout.c
@@ -0,0 +1,50 @@
+/* net/connect4_timeout.c - Make an IPv4 connection with a timeout
+ * Copyright (C) 2004  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <sysdeps.h>
+#include <errno.h>
+#include <net/socket.h>
+#include <unix/nonblock.h>
+
+/** Connect a socket to an IPv4 address, honouring a timeout.
+ *
+ * \note The socket will have nonblocking mode turned on on return from
+ * this function. */
+int socket_connect4_timeout(int sock, const ipv4addr* ip, ipv4port port,
+			    int timeout)
+{
+  iopoll_fd pf;
+  if (!nonblock_on(sock))
+    return 0;
+  if (socket_connect4(sock, ip, port))
+    return 1;
+  if (errno != EINPROGRESS && errno != EWOULDBLOCK)
+    return 0;
+  pf.fd = sock;
+  pf.events = IOPOLL_WRITE;
+  switch (iopoll_restart(&pf, 1, timeout)) {
+  case 0:
+    errno = ETIMEDOUT;
+    return 0;
+  case 1:
+    if (socket_connected(sock))
+      return 1;
+    /* No break, fall through */
+  default:
+    return 0;
+  }
+}
