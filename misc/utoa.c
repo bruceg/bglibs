e54ed27875187889703e7ef572dd01001d884a72
Bruce Guenter <bruce@untroubled.org>
2004-03-23 00:13:05 +0000

    Fixed utoa to write into its buffer instead of before it.

--- misc/utoa.c
+++ misc/utoa.c
@@ -4,7 +4,7 @@ static char utoa_buf[32];
 
 const char* utoa(unsigned long i)
 {
-  char* ptr = utoa_buf - 1;
+  char* ptr = utoa_buf + sizeof utoa_buf - 1;
   *ptr-- = 0;
   while (i >= 10) {
     *ptr-- = (i % 10) + '0';
