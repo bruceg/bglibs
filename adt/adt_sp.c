957f2a2e3a5bb8786c815674d10c68c4e7e9117a
Bruce Guenter <bruce@untroubled.org>
2004-08-23 20:16:51 +0000

    Added missing source from previous commit.

---
+++ adt/adt_sp.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+
+int adt_cmpsp(const char* const* a, const char* const* b)
+{
+  return strcmp(*a, *b);
+}
+
+int adt_copysp(const char** a, const char* const* b)
+{
+  return (*a = strdup(*b)) != 0;
+}
+
+adt_hash_t adt_hashsp(const char* const* p)
+{
+  return adt_hashs(*p);
+}
+
+void adt_freesp(const char** a)
+{
+  free((char*)*a);
+}
