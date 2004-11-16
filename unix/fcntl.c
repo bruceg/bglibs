2d8897d7e00b1f5baa706228309e1d9dfd23dd1b
Bruce Guenter <bruce@untroubled.org>
2004-11-16 21:19:11 +0000

    Added self-tests for the fcntl wrappers.

---
+++ unix/fcntl.c
@@ -0,0 +1,45 @@
+/* unix/fcntl.c - fcntl wrapper self-tests. */
+
+#ifdef SELFTEST_MAIN
+#include "selftest.c"
+#include <unistd.h>
+#include <fcntl.h>
+#include "fcntl.h"
+
+static void fdtest(int fd, int flag)
+{
+  int flags;
+  flags = fcntl(fd, F_GETFD, 0);
+  obuf_putsflush(&outbuf, (flags & flag) ? "set\n" : "cleared\n");
+}
+
+static void fltest(int fd, int flag)
+{
+  int flags;
+  flags = fcntl(fd, F_GETFL, 0);
+  obuf_putsflush(&outbuf, (flags & flag) ? "set\n" : "cleared\n");
+}
+
+MAIN
+{
+  fcntl_fd_off(0, FD_CLOEXEC); fdtest(0, FD_CLOEXEC);
+  fcntl_fd_on(0, FD_CLOEXEC); fdtest(0, FD_CLOEXEC);
+  fcntl_fd_off(0, FD_CLOEXEC); fdtest(0, FD_CLOEXEC);
+  fcntl_fd_on(0, FD_CLOEXEC); fdtest(0, FD_CLOEXEC);
+
+  fcntl_fl_off(0, O_NONBLOCK); fltest(0, O_NONBLOCK);
+  fcntl_fl_on(0, O_NONBLOCK); fltest(0, O_NONBLOCK);
+  fcntl_fl_off(0, O_NONBLOCK); fltest(0, O_NONBLOCK);
+  fcntl_fl_on(0, O_NONBLOCK); fltest(0, O_NONBLOCK);
+}
+#endif
+#ifdef SELFTEST_EXP
+cleared
+set
+cleared
+set
+cleared
+set
+cleared
+set
+#endif
