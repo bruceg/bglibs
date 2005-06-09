67685eb0427d02142a61c5118b1a0b79773aa6a8
Bruce Guenter <bruce@untroubled.org>
2005-06-09 16:39:41 +0000

    Added a wrapper for functions that allocate memory (returning a pointer).

--- msg/wrap.h
+++ msg/wrap.h
@@ -4,5 +4,6 @@
 extern int wrap_exit;
 extern void wrap_chdir(const char*);
 extern void wrap_str(int);
+extern void wrap_alloc(const void*);
 
 #endif
---
+++ msg/wrap_alloc.c
@@ -0,0 +1,25 @@
+#include "msg.h"
+#include "wrap.h"
+
+void wrap_alloc(const void* p)
+{
+  if (p == 0)
+    die_oom(wrap_exit);
+}
+
+#ifdef SELFTEST_MAIN
+#include "selftest.c"
+const char program[] = "testprog";
+const int msg_show_pid = 0;
+void selftest(void)
+{
+  wrap_exit = 0;
+  wrap_alloc(selftest);
+  obuf_putsflush(&outbuf, "worked\n");
+  wrap_alloc(0);
+}
+#endif
+#ifdef SELFTEST_EXP
+worked
+testprog: Fatal: Out of memory
+#endif
