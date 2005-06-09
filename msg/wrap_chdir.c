e723d36878be2bb373b7874f4fea16fc0fca2752
Bruce Guenter <bruce@untroubled.org>
2005-06-09 16:41:56 +0000

    Added self-tests for all wrap functions.

--- msg/wrap_chdir.c
+++ msg/wrap_chdir.c
@@ -8,3 +8,20 @@ void wrap_chdir(const char* dir)
   if (chdir(dir) != 0)
     die3sys(wrap_exit, "Could not chdir to '", dir, "'");
 }
+
+#ifdef SELFTEST_MAIN
+#include "selftest.c"
+const char program[] = "testprog";
+const int msg_show_pid = 0;
+void selftest(void)
+{
+  wrap_exit = 0;
+  wrap_chdir(".");
+  obuf_putsflush(&outbuf, "worked\n");
+  wrap_chdir("/this/dir/should/not/exist");
+}
+#endif
+#ifdef SELFTEST_EXP
+worked
+testprog: Fatal: Could not chdir to '/this/dir/should/not/exist': No such file or directory
+#endif
--- msg/wrap_str.c
+++ msg/wrap_str.c
@@ -6,3 +6,20 @@ void wrap_str(int result)
   if (!result)
     die_oom(wrap_exit);
 }
+
+#ifdef SELFTEST_MAIN
+#include "selftest.c"
+const char program[] = "testprog";
+const int msg_show_pid = 0;
+void selftest(void)
+{
+  wrap_exit = 0;
+  wrap_str(1);
+  obuf_putsflush(&outbuf, "worked\n");
+  wrap_str(0);
+}
+#endif
+#ifdef SELFTEST_EXP
+worked
+testprog: Fatal: Out of memory
+#endif
