f3a0c99443b5c692bc3ef80e6a1a45c06d8748a8
Bruce Guenter <bruce@untroubled.org>
2006-07-27 19:01:01 +0000

    Pointer signedness fixup.

--- adt/adt_sp.c
+++ adt/adt_sp.c
@@ -16,7 +16,7 @@ int adt_copysp(const char** a, const char* const* b)
 
 adt_hash_t adt_hashsp(const char* const* p)
 {
-  return adt_hashs(*p);
+  return adt_hashs((const unsigned char*)*p);
 }
 
 void adt_freesp(const char** a)
