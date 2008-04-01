d2852a56dd04f9b164bfc9fce0f71a1bded39b6e
Bruce Guenter <bruce@untroubled.org>
2008-03-31 22:32:51 -0600

    Completed the signalfd implementation and self-tests.

--- unix/signalfd.c
+++ unix/signalfd.c
@@ -12,7 +12,8 @@ static int fds[2] = { -1, -1 };
 
 static void signalfd_handler(int sig)
 {
-  write(fds[1], &sig, sizeof sig);
+  char signum = sig;
+  write(fds[1], &signum, 1);
 }
 
 /** Initialize the signal file descriptor.
@@ -25,9 +26,9 @@ static void signalfd_handler(int sig)
  * blocking when reading from it.
  *
  * To use the file descriptor, use either \c poll or \c select to
- * determine when it is readable.  When it becomes readable, read an
- * integer-sized block from it.  The value of this data indicates what
- * signal was caught.
+ * determine when it is readable.  When it becomes readable, read a
+ * single byte from it.  The value of this byte indicates what signal
+ * was caught.
  *
  * Due to the global nature of UNIX signal handling, only a single
  * signalfd interface may be active at any time.  Do not call \c
@@ -80,35 +81,41 @@ void puti(const char* word, long i)
 }
 MAIN
 {
-  int buf;
-  int fd = selfpipe_init();
-  pid_t pid = getpid();
-  puti("FD[0] >= 0", fds[0] >= 0);
-  puti("FD[1] >= 0", fds[1] >= 0);
-  puti("FD[0] != FD[1]", fds[0] != fds[1]);
-  puti("Returned FD == FD[0]", fd == fds[0]);
+  char buf[4];
+  int fd;
+  pid_t pid;
+
+  for (fd = 3; fd < 32; ++fd)
+    close(fd);
+  fd = signalfd_init();
+  signalfd_catch(SIGCHLD);
+  puti("FD[0]", fds[0]);
+  puti("FD[1]", fds[1]);
+  puti("Returned FD", fd);
   puti("read before SIGCHLD", read(fd, &buf, sizeof buf));
   puti("errno is EAGAIN", errno == EAGAIN);
+  pid = getpid();
   kill(pid, SIGCHLD);
   puti("read after SIGCHLD", read(fd, &buf, sizeof buf));
+  puti("data is SIGCHLD", buf[0] == SIGCHLD);
   puti("second read", read(fd, &buf, sizeof buf));
   kill(pid, SIGCHLD);
   kill(pid, SIGCHLD);
   puti("read after two SIGCHLDs", read(fd, &buf, sizeof buf));
   puti("second read", read(fd, &buf, sizeof buf));
-  selfpipe_close();
+  signalfd_close();
   puti("read after close", read(fd, &buf, sizeof buf));
   puti("errno is EBADF", errno == EBADF);
 }
 #endif
 #ifdef SELFTEST_EXP
-FD[0] >= 0: 1
-FD[1] >= 0: 1
-FD[0] != FD[1]: 1
-Returned FD == FD[0]: 1
+FD[0]: 3
+FD[1]: 4
+Returned FD: 3
 read before SIGCHLD: -1
 errno is EAGAIN: 1
 read after SIGCHLD: 1
+data is SIGCHLD: 1
 second read: -1
 read after two SIGCHLDs: 2
 second read: -1
