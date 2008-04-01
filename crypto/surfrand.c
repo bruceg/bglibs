8697535351f68a5c9bf9edae2bf66795469b8c6e
Bruce Guenter <bruce@untroubled.org>
2008-04-01 13:16:00 -0600

    Updated copyright notes on the crypto files.

--- crypto/surfrand.c
+++ crypto/surfrand.c
@@ -1,4 +1,5 @@
-/* Copyright (C) 2008  Bruce Guenter <bruce@untroubled.org>
+/* surfrand.c - Random number generator based on SURF
+ * Copyright (C) 2008  Bruce Guenter <bruce@untroubled.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
