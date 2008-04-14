a79700ba68fd3a3f8e8926fbb77330bb92cce7ae
Bruce Guenter <bruce@untroubled.org>
2008-04-13 22:49:13 -0600

    Fixed envstr_unset to remove all instances of the given name.

--- str/env_unset.c
+++ str/env_unset.c
@@ -6,8 +6,13 @@
 void envstr_unset(struct str* env, const char* var)
 {
   const char* found;
-  if ((found = envstr_find(env, var, strlen(var))) != 0)
-    str_spliceb(env, found - env->s, strlen(found) + 1, 0, 0);
+  unsigned long varlen = strlen(var);
+  while ((found = envstr_find(env, var, varlen)) != 0) {
+    unsigned long foundlen = strlen(found) + 1;
+    if (found - env->s + foundlen > env->len - (found - env->s))
+      foundlen = env->len - (found - env->s);
+    str_spliceb(env, found - env->s, foundlen, 0, 0);
+  }
 }
 
 #ifdef SELFTEST_MAIN
@@ -15,14 +20,14 @@ void envstr_unset(struct str* env, const char* var)
 MAIN
 {
   static str env;
-  str_copyb(&env, "A=1\0B=2\0C=3\0", 12);
+  str_copyb(&env, "A=1\0B=2\0C=3\0B=2", 15);
   envstr_unset(&env, "B"); debugstr(&env);
   envstr_unset(&env, "A"); debugstr(&env);
   envstr_unset(&env, "C"); debugstr(&env);
 }
 #endif
 #ifdef SELFTEST_EXP
-len=8 size=16 s=A=1^@C=3^@
-len=4 size=16 s=C=3^@
-len=0 size=16 s=
+len=8 size=32 s=A=1^@C=3^@
+len=4 size=32 s=C=3^@
+len=0 size=32 s=
 #endif
