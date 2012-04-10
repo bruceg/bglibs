fd58cf78f00915ff5293b894aa9548ab1703e3f8
Bruce Guenter <bruce@untroubled.org>
2012-04-10 13:17:35 -0600

    reorg: Remove obsolete err sources

--- err/warn.c
+++
@@ -1,7 +0,0 @@
-#include "err.h"
-
-void warn(const char* a, const char* b, const char* c, const char* d,
-	  const char* e, const char* f, int showsys)
-{
-  err_message("Warning", a, b, c, d, e, f, showsys);
-}
