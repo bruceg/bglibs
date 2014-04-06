f897b39c8e6ca028ac4703f4b377ecbce429888a
Bruce Guenter <bruce@untroubled.org>
2014-04-05 19:46:29 -0600

    net/ipv6: Fix typos in IPV6ADDR constants

--- net/ipv6_addr.c
+++ net/ipv6_addr.c
@@ -1,6 +1,6 @@
 #include "ipv6.h"
 
 /** IPv6 "any" address constant. */
-const ipv6addr IPV6ADDR_ANY = {{ 0,0,0,0,0,0,0,0 }};
+const ipv6addr IPV6ADDR_ANY = {{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }};
 /** IPv6 loopback address constant. */
-const ipv6addr IPV6ADDR_LOOPBACK = {{ 0,0,0,0,0,0,0,1 }};
+const ipv6addr IPV6ADDR_LOOPBACK = {{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 }};
