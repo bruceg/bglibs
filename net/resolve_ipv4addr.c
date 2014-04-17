95ab280c14e07af58d2a14b6f95fea8c187fa8dc
Bruce Guenter <bruce@untroubled.org>
2014-04-16 21:33:32 -0600

    net/resolve: Use the DNS library internally

--- net/resolve_ipv4addr.c
+++ net/resolve_ipv4addr.c
@@ -1,13 +1,22 @@
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include "dns.h"
 #include "resolve.h"
 
 /** Look up the domain name corresponding to an IPv4 address. */
 const char* resolve_ipv4addr(const ipv4addr* addr)
 {
-  struct hostent* ent;
-  h_errno = 0;
-  if ((ent = gethostbyaddr(&addr->addr, 4, AF_INET)) == 0) return 0;
-  return ent->h_name;
+  static struct dns_result out = {0};
+  if (!resolve_calldns(dns_name4, &out, addr))
+    return 0;
+  return out.rr.name[0];
 }
+
+#ifdef SELFTEST_MAIN
+MAIN
+{
+  ipv4addr ip = {{ 69,5,1,51 }};
+  obuf_putf(&outbuf, "s{ => }s{\n}", ipv4_format(&ip), resolve_ipv4addr(&ip));
+}
+#endif
+#ifdef SELFTEST_EXP
+69.5.1.51 => untroubled.org
+#endif
