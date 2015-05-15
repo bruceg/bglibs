501725b9dbdca2c20f913f87039594955614df6e
Bruce Guenter <bruce@untroubled.org>
2015-05-15 14:26:47 -0600

    sys: Need #define _GNU_SOURCE 1 for definition of struct ucred

--- sys/trypeercred.c
+++ sys/trypeercred.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE 1
 #include <sys/types.h>
 #include <sys/socket.h>
 
--- sysdeps-head.h
+++ sysdeps-head.h
@@ -1,6 +1,7 @@
 #ifndef SYS_DEPS__H__
 #define SYS_DEPS__H__
 
+#define _GNU_SOURCE 1
 #include <sys/types.h>
 typedef unsigned char uint8;
 typedef unsigned short uint16;
