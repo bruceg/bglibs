87a72571d72563f566f6ad2e6beda6754a0d883d
Bruce Guenter <bruce@untroubled.org>
2015-08-13 21:54:45 -0600

    include: Remove obsolete cloexec.h and nonblock.h

--- include/cloexec.h
+++
@@ -1,7 +0,0 @@
-#ifndef UNIX__CLOEXEC__H__
-#define UNIX__CLOEXEC__H__
-
-int cloexec_on(int fd);
-int cloexec_off(int fd);
-
-#endif
--- include/nonblock.h
+++
@@ -1,7 +0,0 @@
-#ifndef UNIX__NONBLOCK__H__
-#define UNIX__NONBLOCK__H__
-
-int nonblock_on(int fd);
-int nonblock_off(int fd);
-
-#endif
