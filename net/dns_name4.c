13f8e92b958c0c5db055841b12b57f6895d86ce7
Bruce Guenter <bruce@untroubled.org>
2016-03-03 12:00:10 -0600

    net/dns: Use captive data in the name[46] tests

--- net/dns_name4.c
+++ net/dns_name4.c
@@ -20,18 +20,54 @@ int dns_name4_r(struct dns_transmit* tx, struct dns_result* out, const ipv4addr 
 DNS_R_FN_WRAP(name4, const ipv4addr*)
 
 #ifdef SELFTEST_MAIN
-struct dns_result out = {0};
+#include "dns-responder.c"
+DUMP {
+  int i;
+  for (i = 0; i < count; i++) {
+    obuf_putc(&outbuf, ' ');
+    obuf_puts(&outbuf, rr->name[i]);
+  }
+  NL();
+}
+RESPONSE responses[] = {
+  { 1, 1, 0, {
+      { "\300\014", 2, DNS_T_PTR, DNS_C_IN, 86400, "\003one\007example\003com\000", 17 },
+      { "\300\014", 2, DNS_T_NS, DNS_C_IN, 123456, "\003ns1\007example\003org\000", 17 },
+    }
+  },
+  { 1, 1, 0, {
+      { "\300\014", 2, DNS_T_PTR, DNS_C_IN, 86400, "\003two\007example\003com\000", 17 },
+      { "\300\014", 2, DNS_T_NS, DNS_C_IN, 123456, "\003ns1\007example\003com\000", 17 },
+    }
+  },
+};
+void doit(const ipv4addr* addr)
+{
+  struct dns_result out = {0};
+  debugfn(dns_name4(&out, addr));
+  obuf_putf(&outbuf, "s{: count=}d{\n}", ipv4_format(addr), out.count);
+  dump_rrs(out.count, &out.rr);
+  obuf_flush(&outbuf);
+}
 MAIN
 {
-  ipv4addr ip = {{ 69,5,1,51 }};
-  int i;
-
-  debugfn(dns_name4(&out, &ip));
-  for (i = 0; i < out.count; ++i)
-    obuf_putf(&outbuf, "d{: \"}s{\"\n}", i, out.rr.name[i]);
+  ipv4addr addr1 = {{ 1,4,2,8 }};
+  ipv4addr addr2 = {{ 9,8,7,6 }};
+  start_dns_responder(responses, 2);
+  doit(&addr1);
+  doit(&addr2);
+  wait_dns_responder();
 }
 #endif
 #ifdef SELFTEST_EXP
+38: ID=XX QR=0 opcode=0 AA=0 TC=0 RD=1 RA=0 Z=0 RCODE=0 QDCOUNT=1 ANCOUNT=0 NSCOUNT=0 ARCOUNT=0
+Question: 8.2.4.1.in-addr.arpa. QTYPE=12 QCLASS=1
 result=0
-0: "untroubled.org"
+1.4.2.8: count=1
+ one.example.com
+38: ID=XX QR=0 opcode=0 AA=0 TC=0 RD=1 RA=0 Z=0 RCODE=0 QDCOUNT=1 ANCOUNT=0 NSCOUNT=0 ARCOUNT=0
+Question: 6.7.8.9.in-addr.arpa. QTYPE=12 QCLASS=1
+result=0
+9.8.7.6: count=1
+ two.example.com
 #endif
--- net/dns_name6.c
+++ net/dns_name6.c
@@ -20,27 +20,54 @@ int dns_name6_r(struct dns_transmit *tx, struct dns_result* out, const ipv6addr*
 DNS_R_FN_WRAP(name6, const ipv6addr*)
 
 #ifdef SELFTEST_MAIN
-struct dns_result out = {0};
-void doit(const char* addr)
-{
-  ipv6addr ip;
+#include "dns-responder.c"
+DUMP {
   int i;
-
-  ipv6_scan(addr, &ip);
-  debugfn(dns_name6(&out, &ip));
-  for (i = 0; i < out.count; ++i)
-    obuf_putf(&outbuf, "d{: \"}s{\"\n}", i, out.rr.name[i]);
+  for (i = 0; i < count; i++) {
+    obuf_putc(&outbuf, ' ');
+    obuf_puts(&outbuf, rr->name[i]);
+  }
+  NL();
+}
+RESPONSE responses[] = {
+  { 1, 1, 0, {
+      { "\300\014", 2, DNS_T_PTR, DNS_C_IN, 86400, "\003one\007example\003com\000", 17 },
+      { "\300\014", 2, DNS_T_NS, DNS_C_IN, 123456, "\003ns1\007example\003org\000", 17 },
+    }
+  },
+  { 1, 1, 0, {
+      { "\300\014", 2, DNS_T_PTR, DNS_C_IN, 86400, "\003two\007example\003com\000", 17 },
+      { "\300\014", 2, DNS_T_NS, DNS_C_IN, 123456, "\003ns1\007example\003com\000", 17 },
+    }
+  },
+};
+void doit(const ipv6addr* addr)
+{
+  struct dns_result out = {0};
+  debugfn(dns_name6(&out, addr));
+  obuf_putf(&outbuf, "s{: count=}d{\n}", ipv6_format(addr), out.count);
+  dump_rrs(out.count, &out.rr);
+  obuf_flush(&outbuf);
 }
 MAIN
 {
-  doit("::ffff:69.5.1.51");
-  doit("2620::861:ed1a:0:0:0:1");
+  ipv6addr addr1 = {{ 0,1,2,3,4,5,6,7,8,9,0xa,0xb,0xc,0xd,0xe,0xf }};
+  ipv6addr addr2 = {{ 0,0,0,0,0,0,0,0,0,0,0xff,0xff,0xab,10,42,99 }};
+  start_dns_responder(responses, 2);
+  doit(&addr1);
+  doit(&addr2);
+  wait_dns_responder();
 }
-
 #endif
 #ifdef SELFTEST_EXP
+90: ID=XX QR=0 opcode=0 AA=0 TC=0 RD=1 RA=0 Z=0 RCODE=0 QDCOUNT=1 ANCOUNT=0 NSCOUNT=0 ARCOUNT=0
+Question: f.0.e.0.d.0.c.0.b.0.a.0.9.0.8.0.7.0.6.0.5.0.4.0.3.0.2.0.1.0.0.0.ip6.arpa. QTYPE=12 QCLASS=1
 result=0
-0: "untroubled.org"
+1:203:405:607:809:a0b:c0d:e0f: count=1
+ one.example.com
+43: ID=XX QR=0 opcode=0 AA=0 TC=0 RD=1 RA=0 Z=0 RCODE=0 QDCOUNT=1 ANCOUNT=0 NSCOUNT=0 ARCOUNT=0
+Question: 99.42.10.171.in-addr.arpa. QTYPE=12 QCLASS=1
 result=0
-0: "text-lb.eqiad.wikimedia.org"
+::ffff:171.10.42.99: count=1
+ two.example.com
 #endif
